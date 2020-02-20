d0d580f89fe25fc435d717ed8e2f9abc0fe1b17d
mina <mina0774@hanmail.net>
2020-02-20 15:06:19 +0900

    link index

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,10 @@ from django.utils import timezone
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    parent_title_choices=(('Profile','Profile'),('Activity','Activity'),('Project','Project'))
+    parent_title=models.CharField(max_length=30,choices=parent_title_choices,default='Profile')
+    index_title_choices=(('Profile',(('Skill','Skill'),)),('Activity',(('UCI Undergraduate Researcher','UCI Undergraduate Researcher'),('Coding HelpDesk','Coding HelpDesk'),('Heronation InternShip','Heronation InternShip'))),('Project',(('Face Recogntion','Face Recognition'),('Mu:Ping App','Mu:Ping App'),('Ang-Bob App','Ang-Bob App'),('Flex-Bison Interpreter','Flex-Bison Interpreter'),('Z-shop App','Z-shop App'),)))
+    index_title=models.CharField(max_length=30,choices=index_title_choices,default='Skill')
     title = models.CharField(max_length=200)
     period=models.CharField(max_length=50)
     text = models.TextField()
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    profile=Post.objects.filter(parent_title__contains="Skill")
+    activity=Post.objects.filter(parent_title__contains="Activity")
+    project=Post.objects.filter(parent_title__contains="Project")
+    return render(request, 'blog/post_list.html', {'posts': posts,'profile':profile,"activity":activity,"project":project})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    profile=Post.objects.filter(parent_title__contains="Skill")
+    activity=Post.objects.filter(parent_title__contains="Activity")
+    project=Post.objects.filter(parent_title__contains="Project")
+    return render(request, 'blog/post_detail.html', {'post': post,'profile':profile,"activity":activity,"project":project})
