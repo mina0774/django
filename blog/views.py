53a5ed50eac7a2444b139d39831ca4e1c769cf83
mina <mina0774@hanmail.net>
2020-02-21 12:16:59 +0900

    posting editing function

--- blog/views.py
+++ blog/views.py
@@ -2,24 +2,48 @@ from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
+from django.shortcuts import redirect
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    profile=Post.objects.filter(parent_title__contains="Skill")
+    profile=Post.objects.filter(parent_title__contains="Profile")
     activity=Post.objects.filter(parent_title__contains="Activity")
     project=Post.objects.filter(parent_title__contains="Project")
     return render(request, 'blog/post_list.html', {'posts': posts,'profile':profile,"activity":activity,"project":project})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    profile=Post.objects.filter(parent_title__contains="Skill")
+    profile=Post.objects.filter(parent_title__contains="Profile")
     activity=Post.objects.filter(parent_title__contains="Activity")
     project=Post.objects.filter(parent_title__contains="Project")
     return render(request, 'blog/post_detail.html', {'post': post,'profile':profile,"activity":activity,"project":project})
 
 def post_new(request):
-    form=PostForm()
-    profile=Post.objects.filter(parent_title__contains="Skill")
+    profile=Post.objects.filter(parent_title__contains="Profile")
     activity=Post.objects.filter(parent_title__contains="Activity")
     project=Post.objects.filter(parent_title__contains="Project")
+    if request.method == "POST":
+        form=PostForm(request.POST)
+        if form.is_valid():
+            post=form.save(commit=False)
+            post.author=request.user
+            post.published_date=timezone.now()
+            post.save()
+            return redirect('post_detail',pk=post.pk)
+    else:
+        form=PostForm()
     return render(request,'blog/post_edit.html',{'form':form,'profile':profile,"activity":activity,"project":project})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
