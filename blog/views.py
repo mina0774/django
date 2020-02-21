1dc6c4d9db2d06a3472b9d0c4a4cbb473e0d99e2
mina <mina0774@hanmail.net>
2020-02-21 16:04:58 +0900

    implement deleting function

--- blog/views.py
+++ blog/views.py
@@ -3,25 +3,26 @@ from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 from django.shortcuts import redirect
+from django.db.models import Q
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    profile=Post.objects.filter(parent_title__contains="Profile")
-    activity=Post.objects.filter(parent_title__contains="Activity")
-    project=Post.objects.filter(parent_title__contains="Project")
+    profile=Post.objects.filter(parent_title__contains="Profile",published_date__lte=timezone.now())
+    activity=Post.objects.filter(parent_title__contains="Activity",published_date__lte=timezone.now())
+    project=Post.objects.filter(parent_title__contains="Project",published_date__lte=timezone.now())
     return render(request, 'blog/post_list.html', {'posts': posts,'profile':profile,"activity":activity,"project":project})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    profile=Post.objects.filter(parent_title__contains="Profile")
-    activity=Post.objects.filter(parent_title__contains="Activity")
-    project=Post.objects.filter(parent_title__contains="Project")
+    profile=Post.objects.filter(parent_title__contains="Profile",published_date__lte=timezone.now())
+    activity=Post.objects.filter(parent_title__contains="Activity",published_date__lte=timezone.now())
+    project=Post.objects.filter(parent_title__contains="Project",published_date__lte=timezone.now())
     return render(request, 'blog/post_detail.html', {'post': post,'profile':profile,"activity":activity,"project":project})
 
 def post_new(request):
-    profile=Post.objects.filter(parent_title__contains="Profile")
-    activity=Post.objects.filter(parent_title__contains="Activity")
-    project=Post.objects.filter(parent_title__contains="Project")
+    profile=Post.objects.filter(parent_title__contains="Profile",published_date__lte=timezone.now())
+    activity=Post.objects.filter(parent_title__contains="Activity",published_date__lte=timezone.now())
+    project=Post.objects.filter(parent_title__contains="Project",published_date__lte=timezone.now())
     if request.method == "POST":
         form=PostForm(request.POST)
         if form.is_valid():
@@ -35,6 +36,9 @@ def post_new(request):
     return render(request,'blog/post_edit.html',{'form':form,'profile':profile,"activity":activity,"project":project})
 
 def post_edit(request, pk):
+    profile=Post.objects.filter(parent_title__contains="Profile",published_date__lte=timezone.now())
+    activity=Post.objects.filter(parent_title__contains="Activity",published_date__lte=timezone.now())
+    project=Post.objects.filter(parent_title__contains="Project",published_date__lte=timezone.now())
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
         form = PostForm(request.POST, instance=post)
@@ -46,4 +50,9 @@ def post_edit(request, pk):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})
+    return render(request, 'blog/post_edit.html', {'form': form,'profile':profile,"activity":activity,"project":project})
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
