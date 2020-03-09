bbc1e8001e127f58978d6ea7b1be1081d422e5e1
mina <mina0774@hanmail.net>
2020-03-09 12:23:34 +0900

    disqus를 이용한 대댓글

--- blog/models.py
+++ blog/models.py
@@ -43,9 +43,6 @@ class Comment(models.Model):
     text=models.TextField()
     created_date=models.DateTimeField(default=timezone.now)
     approved_comment=models.BooleanField(default=False)
-    groupno=models.IntegerField(default=0)
-    orderno=models.IntegerField(default=0)
-    depth=models.IntegerField(default=0)
 
     def approve(self):
         self.approved_comment=True
--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -15,7 +15,6 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 LOGIN_REDIRECT_URL = '/'
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
